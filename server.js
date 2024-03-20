8fff4d85e95f8a45df8bab76c39edf91619fe09c
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-03-20 19:33:26 +0200

    link firebase and add form sharing

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
+
+//firebase admin
+
+var serviceAccount = require("./clothing-site-b85f5-firebase-adminsdk-krhrz-ddf076602c.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 let staticPath = path.join(__dirname, "public");
 
 const app = express();
@@ -15,6 +24,25 @@ app.get("/", (req, res) => {
 
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(staticPath, "signup.html"));
+})
+
+app.post('/signup', (req, res) => {
+    let { name, email, password, number, tac, notification } = req.body;
+
+    // form validations
+    if(name.length < 3){
+        return res.json({'alert': 'name must be 3 letters long'});
+    } else if(!email.length){
+        return res.json({'alert': 'enter your email'});
+    } else if(password.length < 8){
+        return res.json({'alert': 'password should be 8 letters long'});
+    } else if(!number.length){
+        return res.json({'alert': 'enter your phone number'});
+    } else if(!Number(number) || number.length < 10){
+        return res.json({'alert': 'invalid number, please enter valid one'});
+    } else if(!tac){
+        return res.json({'alert': 'you must agree to our terms and conditions'});
+    }
 })
 
 
