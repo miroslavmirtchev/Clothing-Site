48248a141f90eb92194be7f4a46956732054458d
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-03-27 22:07:50 +0200

    move file and change path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 
 // firebase admin setup
-let serviceAccount = require(".env/clothing-site-b85f5-firebase-adminsdk-krhrz-6e08f768f3");
+let serviceAccount = require("clothing-site-b85f5-firebase-adminsdk-krhrz-6e08f768f3");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
