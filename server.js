9415c60f113d6fe20fa6ab425ab368c848b8eb7c
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-03-27 22:00:53 +0200

    Cleaned up for code visibility and added seller redirect

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
+// importing packages
 const express = require('express');
 const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
 
-//firebase admin
-
-let serviceAccount = require("./.env/clothing-site-b85f5-firebase-adminsdk-krhrz-6e08f768f3.json");
+// firebase admin setup
+let serviceAccount = require("path to your firebase seceret key");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
+let db = admin.firestore();
 
-let db=admin.firestore();
-
+// declare static path
 let staticPath = path.join(__dirname, "public");
 
+//intializing express.js
 const app = express();
 
+//middlewares
 app.use(express.static(staticPath));
-app.use(express.json);
+app.use(express.json());
 
+//routes
+//home route
 app.get("/", (req, res) => {
     res.sendFile(path.join(staticPath, "index.html"));
 })
 
+//signup route
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(staticPath, "signup.html"));
 })
@@ -69,45 +74,66 @@ app.post('/signup', (req, res) => {
                 })
             }
         })
-    
-    res.json('data received');
 })
 
-//login route
-app.get('/login', (req,res) =>{
-    res.sendFile(path.join(staticPath,"login.html"))
+// login route
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(staticPath, "login.html"));
 })
 
-app.post('/login',(res,req) =>{
-    let{email,password}= req.body;
-    
+app.post('/login', (req, res) => {
+    let { email, password } = req.body;
+
     if(!email.length || !password.length){
-        return res.json({'alert':'fill all the inputs'});
+        return res.json({'alert': 'fill all the inputs'})
     }
-    
+
     db.collection('users').doc(email).get()
-        .then(user =>{//check if email exists
-            if(!user.exists) {
-                return res.json({'alert': 'log in email does not exist'})
-            }
-            else{
-                bcrypt.compare(password, user.data().password,(err,result) =>{
+        .then(user => {
+            if(!user.exists){ // if email does not exists
+                return res.json({'alert': 'log in email does not exists'})
+            } else{
+                bcrypt.compare(password, user.data().password, (err, result) => {
                     if(result){
-                        let data=user.data();
+                        let data = user.data();
                         return res.json({
-                            name:data.name,
-                            email:data.email,
-                            seller:data.seller,
+                            name: data.name,
+                            email: data.email,
+                            seller: data.seller,
                         })
-                    }
-                    else{
-                        return res.json({'alert':'password is incorrect'});
+                    } else{
+                        return res.json({'alert': 'password is incorrect'});
                     }
                 })
             }
         })
 })
 
+// seller route
+app.get('/seller', (req, res) => {
+    res.sendFile(path.join(staticPath, "seller.html"));
+})
+
+app.post('/seller', (req, res) => {
+    let { name, about, address, number, tac, legit, email } = req.body;
+    if(!name.length || !address.length || !about.length || number.length < 10 || !Number(number)){
+        return res.json({'alert':'some inforamation(s) is/are invalid'});
+    } else if(!tac || !legit){
+        return res.json({'alert': 'you must agree to our terms and conditions'})
+    } else{
+        // update users seller status here.
+        db.collection('sellers').doc(email).set(req.body)
+            .then(data => {
+                db.collection('users').doc(email).update({
+                    seller: true
+                }).then(data => {
+                    res.json(true);
+                })
+            })
+    }
+})
+
+// 404 route
 app.get('/404', (req, res) => {
     res.sendFile(path.join(staticPath, "404.html"));
 })
@@ -116,8 +142,6 @@ app.use((req, res) => {
     res.redirect('/404');
 })
 
-
 app.listen(3000, () => {
     console.log('listening on port 3000.......');
 })
-
