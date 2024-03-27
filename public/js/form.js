2def460ef410ee4c51b0392a36989f7202fc2f5e
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-03-27 19:29:33 +0200

    make changes so login works

--- public/js/form.js
+++ public/js/form.js
@@ -12,39 +12,30 @@ const loader = document.querySelector('.loader');
 
 // select inputs 
 const submitBtn = document.querySelector('.submit-btn');
-const name = document.querySelector('#name');
+const name = document.querySelector('#name') || null;
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const number = document.querySelector('#number');
-const tac = document.querySelector('#terms-and-cond');
-const notification = document.querySelector('#notification');
+const number = document.querySelector('#number') || null;
+const tac = document.querySelector('#terms-and-cond') || null;
+const notification = document.querySelector('#notification') || null;
 
 submitBtn.addEventListener('click', () => {
-    if (name.value.length < 3) {
-        showAlert('name must be 3 letters long');
-    } else if (!email.value.length) {
-        showAlert('enter your email');
-    } else if (password.value.length < 8) {
-        showAlert('password should be 8 letters long');
-    } else if (!number.value.length) {
-        showAlert('enter your phone number');
-    } else if (!Number(number.value) || number.value.length < 10) {
-        showAlert('invalid number, please enter valid one');
-    } else if (!tac.checked) {
-        showAlert('you must agree to our terms and conditions');
-    } else {
-        // submit form
-        loader.style.display = 'block';
-        sendData('/signup', {
-            name: name.value,
-            email: email.value,
-            password: password.value,
-            number: number.value,
-            tac: tac.checked,
-            notification: notification.checked,
-            seller: false
-        })
+    if(name!=null){
+        if(name.value.length < 3){
+            return res.json({'alert': 'name must be 3 letters long'});
+        } else if(!email.length){
+            return res.json({'alert': 'enter your email'});
+        } else if(password.length < 8){
+            return res.json({'alert': 'password should be 8 letters long'});
+        } else if(!number.length){
+            return res.json({'alert': 'enter your phone number'});
+        } else if(!Number(number) || number.length < 10){
+            return res.json({'alert': 'invalid number, please enter valid one'});
+        } else if(!tac){
+            return res.json({'alert': 'you must agree to our terms and conditions'});
+        }
     }
+    })
 
     //send data
 const sendData = (path, data) => {
