78d4edefe31bda68e4d6753cc70c03f61e007ef0
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-03-28 20:34:03 +0200

    Create functionality for search bar

---
+++ public/js/search.js
@@ -0,0 +1,6 @@
+﻿const searchKey = decodeURI(location.pathname.split('/').pop());
+
+const searchSpanElement = document.querySelector('#search-key');
+searchSpanElement.innerHTML = searchKey;
+
+getProducts(searchKey).then(data => createProductCards(data, '.card-container'));
